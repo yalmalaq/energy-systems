9b78d1a96cae2abf5baf3a6dc9ba6048de58019e
Yazeed Almalaq <124050604+yalmalaq@users.noreply.github.com>
2024-06-11 21:49:12 +0100

    Update all_software_measuring.c
    ails + charge counter + battery state on all apps

--- software_solutions/all_software_measuring.c
+++ software_solutions/all_software_measuring.c
@@ -4,7 +4,6 @@
 #include <fcntl.h>
 #include <time.h>
 
-
 double read_value(const char* file_path) {
     FILE *file = fopen(file_path, "r");
     if (!file) {
@@ -16,21 +15,16 @@ double read_value(const char* file_path) {
     return value;
 }
 
-void swipe(int startX, int startY, int endX, int endY) {
-    char command[100];
-    snprintf(command, sizeof(command), "input touchscreen swipe %d %d %d %d", startX, startY, endX, endY);
-    system(command);
-}
-
-void tap(int x, int y) {
-    char command[100];
-    snprintf(command, sizeof(command), "input touchscreen tap %d %d", x, y);
-    system(command);
-}
 
 void openApp(const char* appActivity) {
     char command[150];
     snprintf(command, sizeof(command), "am start -n %s", appActivity);
+    system(command);
+}
+
+void openurl(const char* appActivity) {
+    char command[150];
+    snprintf(command, sizeof(command), "am start -a android.intent.action.VIEW -d %s", appActivity);
     system(command);
 }
 
@@ -72,6 +66,40 @@ long read_energy_values() {
     return 0;
 }
 
+void set_charging_charge_counter(int on) {
+    int fd = open("/sys/class/power_supply/usb/device/usb_limit_sink_enable", O_WRONLY);
+    if (fd < 0) {
+        perror("Failed to open usb_limit_sink_enable");
+        exit(EXIT_FAILURE);
+    }
+    char buf[] = "0\n";
+    buf[0] = on ? '0' : '1';
+    ssize_t size = write(fd, buf, sizeof(buf));
+    if (size < (ssize_t)sizeof(buf)) {
+        perror("Failed to write to usb_limit_sink_enable");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    close(fd);
+}
+
+void set_charging_battery_state(int on)
+{
+     int fd = open("/sys/class/power_supply/main-charger/online", O_WRONLY);
+    if (fd < 0) {
+        perror("Failed to open usb_limit_sink_enable");
+        exit(EXIT_FAILURE);
+    }
+    char buf[] = "0\n";
+    buf[0] = on ? '1' : '0';
+    int size = write(fd, buf, sizeof(buf));
+    if (size < sizeof(buf)) {
+                perror("no!");
+                exit(-1);
+            }
+            close(fd);
+}
+
 
 
 
@@ -82,79 +110,144 @@ double get_current_time() {
 }
 
 
+
+void social_media() {
+    openApp("com.twitter.android/com.twitter.android.StartActivity");
+    
+    for (int i = 0; i < 9; ++i) {
+        system("input touchscreen swipe 100 1500 100 200");
+        sleep(2);
+    }
+    sleep(2);
+    system("input touchscreen swipe 100 200 100 1500");
+    
+    for (int i = 0; i < 2; ++i) {
+        system("input touchscreen tap 990 2100");
+        sleep(2);
+    }
+        system("input touchscreen tap 990 2100");
+        sleep(2);
+        system("input touchscreen tap 600 1900");
+        sleep(2);
+        system("input touchscreen tap 850 1700");
+        sleep(2);
+        system("input touchscreen tap 1000 200");
+        sleep(2);
+    
+    
+    openApp("com.zhiliaoapp.musically/com.ss.android.ugc.aweme.splash.SplashActivity");
+    
+    for (int i = 0; i < 5; ++i) {
+        system("input touchscreen swipe 100 1500 100 200");
+        sleep(5);
+    }
+}
+
+void games() {
+    openApp("com.JindoBlu.OfflineGames/com.unity3d.player.UnityPlayerActivity");
+    sleep(20);
+    
+    system("input touchscreen tap 100 1500");
+    sleep(2);
+    
+    system("input touchscreen tap 540 1900");
+    sleep(2);
+    
+    system("input touchscreen tap 500 1700");
+    sleep(2);
+    
+    for (int i = 0; i < 30; ++i) {
+        system("input touchscreen tap 540 960");
+        sleep(1);
+    }
+
+}
+    
+
+void web_browsing() {
+    openurl("https://youtube.com");
+    sleep(5);
+    
+    for (int i = 0; i < 5; ++i) {
+        system("input touchscreen swipe 100 1500 100 200");
+        sleep(2);
+    }
+    
+    system("input touchscreen tap 100 1500");
+    sleep(5);
+    
+    for (int i = 0; i < 3; ++i) {
+        system("input touchscreen swipe 100 1500 100 200");
+        sleep(2);
+    }
+    sleep(5);
+    
+    openurl("https://reddit.com");
+    sleep(5);
+    
+    for (int i = 0; i < 5; ++i) {
+        system("input touchscreen swipe 100 1500 100 200");
+        sleep(2);
+    }
+    
+    system("input touchscreen tap 100 1500");
+    sleep(2);
+    
+    for (int i = 0; i < 5; ++i) {
+        system("input touchscreen swipe 100 1500 100 200");
+        sleep(2);
+    }
+}
+
+
 int main() {
+    set_charging_battery_state(0);
+    set_charging_charge_counter(0);
+    
+    
     double initial_power = measure_power_consumption();
     double start_time = get_current_time();
     long start_value = read_counter();
     printf("Start rails data:\n");
     read_energy_values();
     
-    openApp("com.twitter.android/com.twitter.android.StartActivity");
 
-    // doing realistic behav on X app
-    swipe(100, 1500, 100, 200);
-    sleep(2);
-    swipe(100, 1500, 100, 200);
-    sleep(2);
-    swipe(100, 1500, 100, 200);
-    sleep(2);
-    swipe(100, 1500, 100, 200);
-    sleep(2);
-    swipe(100, 1500, 100, 200);
-    sleep(2);
-    swipe(100, 1500, 100, 200);
-    sleep(2);
-    swipe(100, 1500, 100, 200);
-    sleep(2);
-    swipe(100, 1500, 100, 200);
-    sleep(2);
-    swipe(100, 200, 100, 1500);
-    sleep(2);
-    tap(990, 2100);
-    sleep(2);
-    tap(990, 2100);
-    sleep(2);
-    tap(600, 1900);
-    sleep(2);
-    tap(850, 1700);
-    sleep(2);
-    tap(1000, 200);
-    sleep(2);
 
-    openApp("com.zhiliaoapp.musically/com.ss.android.ugc.aweme.splash.SplashActivity");
+    // social_media app ( X + TikTook)
+    //social_media();
+    
+    // Offiline games
+   // games();
 
-    swipe(100, 1500, 100, 200);
-    sleep(5);
-    swipe(100, 1500, 100, 200);
-    sleep(5);
-    swipe(100, 1500, 100, 200);
-    sleep(5);
-    swipe(100, 1500, 100, 200);
-    sleep(5);
-    swipe(100, 1500, 100, 200);
-    sleep(5);
-    swipe(100, 1500, 100, 200);
-    sleep(5);
+    web_browsing();
+    
+
 
     double end_time = get_current_time();
  
 
     double final_power = measure_power_consumption();
-    printf("power_batterystate: %.6f W\n", final_power);
+//    printf("power_batterystate: %.6f W\n", final_power);
     
     double duration = end_time - start_time;
-    printf("time: %.6f seconds\n", duration);
+//    printf("time: %.6f seconds\n", duration);
     
     double energy_consumed = (initial_power + final_power) / 2 * duration;
-    printf("energy_batterystate: %.6f J\n", energy_consumed);
+//    printf("energy_batterystate: %.6f J\n", energy_consumed);
     long end_value = read_counter();
     
     double energy_charge_counter = 0.0138 *  (end_value - start_value);
     double power_charge_counter = energy_charge_counter / duration;
-    printf("Power_charge_counter Energy_charge_chounter Time\n%lf %lf %lf\n", power_charge_counter, energy_charge_counter, duration);
     
     printf("\nEnd rails data:\n");
     read_energy_values();
+    
+    printf("power_batterystate energy_batterystate Time\n%lf %lf %lf\n", final_power, energy_consumed, duration);
+    
+    printf("Power_charge_counter Energy_charge_chounter Time\n%lf %lf %lf\n", power_charge_counter, energy_charge_counter, duration);
+    
+    set_charging_charge_counter(1);
+    set_charging_battery_state(1);
     return 0;
 }
 
